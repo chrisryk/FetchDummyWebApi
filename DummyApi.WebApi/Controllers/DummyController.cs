31f606f6971a0196044043fc1c31ef040814a063
Krzysztof Rykowski <rykowski.krzysztof@gmail.com>
2024-03-14 15:50:23 +0100

    Add project files.

---
+++ DummyApi.WebApi/Controllers/DummyController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DummyApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetData([FromQuery] DummyParameters parameters)
+        {
+            var baseUrl = "https://dummyapi.io/";
+            var path = "data/v1/user";
+            var filter = $"?page={parameters.Page}&limit={parameters.Limit}";
+            var idHeader = "65eec7d4f88b2863bdc09cc1";
+
+            var client = new HttpClient();
+
+            client.BaseAddress = new Uri(baseUrl);
+            client.DefaultRequestHeaders.Add("app-id", idHeader);
+            var response = await client.GetAsync(path + filter);
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            return Ok(result);
+        }
+    }
+}
---
+++ DummyApi.WebApi/DummyParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace DummyApi.WebApi
+{
+    public class DummyParameters
+    {
+        public int Page { get; set; } = 1;
+        public int Limit { get; set; } = 10;
+    }
+}
